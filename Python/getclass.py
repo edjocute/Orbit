10b81304f971c94e785444d6714eac279c376624
edjocute <edjocute@hotmail.com>
2017-05-13 18:25:04 -0400

    Fix functions in getclass.py

--- Python/getclass.py
+++ Python/getclass.py
@@ -5,6 +5,8 @@ import os,sys, getopt
 sys.path.append('/n/ghernquist/kchua/Orbit/201-code-C-Mar2016/python')
 import classify
 
+varfile=['var.hdf5','varh.hdf5']
+
 #plt.interactive(False)
 def intclass(filedir,components,rotation,doclass=True,makeplot=False):
     if doclass:
@@ -69,58 +71,53 @@ def plotsingle(filedir,lin=False):
         fig.tight_layout()
         return fig
 
-def plotfin(filedir,components,nbins=10,whichdist=1,ls='-'):
+def plotfin(filedir,components,nbins=10,whichdist=1,ls='-',\
+        range=[0.05,1.]):
     import matplotlib as mpl
     mpl.use('Agg')
     import matplotlib.pyplot as plt
+
     if filedir[-5:]=='.hdf5':
         filename=filedir
         dirt=filedir.split('classout')[0]
     else:
         filename=filedir+'/classout.hdf5'
+
     with tables.open_file(filename,'r') as f:
-        if components==1:
-            varfile='var.hdf5'
-        elif components==2:
-            varfile='varh.hdf5'
-        with tables.open_file(dirt+varfile,'r') as var:
+        try:
+            Ncom=f.root.Ncomponents[0]
+        except tables.NoSuchNodeError:
+            Ncom=components
+        assert Ncom in [0,1]
+
+        with tables.open_file(dirt+varfile[Ncom],'r') as var:
             rvir=var.root.Rvir[:]/classify.h*classify.kpc
         dist=f.root.avgdist[:,whichdist]/rvir
         C=f.root.classification[:]
 
-        edges=np.logspace(-1.7,0,nbins+1)
+        edges=np.logspace(np.log10(range[0]),np.log10(range[1]),nbins+1)
         x=(edges[:-1]+edges[1:])/2
+        allbox,alltube,allirr=calcfrac(C,dist,edges)
 
         fig,ax=plt.subplots(2,1,figsize=(6,10))
-
-        wbox=np.where( (C==4) | (C==5))[0]
-        wtube=np.where( (C==1) | (C==2) | (C==3))[0]
-        wirr=np.where(C==0)[0]
-        box=np.array([bin.sum() for bin in ((edges[i]<dist[wbox]) & (dist[wbox]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        tube=np.array([bin.sum() for bin in ((edges[i]<dist[wtube]) & (dist[wtube]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        irr=np.array([bin.sum() for bin in ((edges[i]<dist[wirr]) & (dist[wirr]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        allbox=box/(box+tube+irr)
-        alltube=tube/(box+tube+irr)
-        allirr=1.-(box+tube)/(box+tube+irr)
-
-        ax[0].semilogx(x,allbox,c='k',ls=ls)
-        ax[0].semilogx(x,alltube,c='b',ls=ls)
-        ax[0].semilogx(x,allirr,c='r',ls=ls)
+        for w,c in [[allbox,'k'],[alltube,'b'],[allirr,'r']]:
+            ax[0].semilogx(x,w,c=c,ls=ls)
         ax[0].legend(['Box','Tube','Irr'],loc=2)
-
+        ax[0].set_xlabel(r'$r/R_{200}$')
+        ax[0].set_ylabel('Orbit Fraction')
         ax[1].loglog(dist,1-f.root.totE[:,1]/f.root.totE[:,0],'.')
         ax[1].set_xlabel('Avg_Dist')
         ax[1].set_ylabel(r'$1-\Delta E/E_0$')
-
         fig.tight_layout()
         return fig
 
-def plotall(filedir,whichdist,nbins=10,components=1,filename='classout.hdf5',ls='-',ploterr=False):
+def plotall(filedir,components,nbins=10,whichdist=1,\
+        filename='classout.hdf5',ls='-',ploterr=False,range=[0.05,1.]):
     import matplotlib.pyplot as plt
     import glob
     per=np.percentile
     allgroups=glob.glob(filedir+'/GROUP_*/')
-    edges=np.logspace(-1.7,0,nbins+1)
+    edges=np.logspace(np.log10(range[0]),np.log10(range[1]),nbins+1)
     x=(edges[:-1]+edges[1:])/2
 
     if len(allgroups)>0:
@@ -137,54 +134,60 @@ def plotall(filedir,whichdist,nbins=10,components=1,filename='classout.hdf5',ls=
             dirt=allgroups[i]
         else:
             dirt='./'
-        if components==1:
-            varfile='var.hdf5'
-        elif components==2:
-            varfile='varh.hdf5'
-        with tables.open_file(dirt+varfile,'r') as var:
-            rvir=var.root.Rvir[:]/classify.h*classify.kpc
         with tables.open_file(dirt+'/'+filename,'r') as f:
-            dist=f.root.avgdist[:,whichdist]/rvir
-            C=f.root.classification[:]
+            try:
+                Ncom=f.root.Ncomponents[0]
+            except tables.NoSuchNodeError:
+                Ncom=components
+            assert Ncom in [0,1]
 
-        wbox=np.where( (C==4) | (C==5))[0]
-        wtube=np.where( (C==1) | (C==2) | (C==3))[0]
-        wirr=np.where(C==0)[0]
-        box=np.array([bin.sum() for bin in ((edges[i]<dist[wbox]) & (dist[wbox]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        tube=np.array([bin.sum() for bin in ((edges[i]<dist[wtube]) & (dist[wtube]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        irr=np.array([bin.sum() for bin in ((edges[i]<dist[wirr]) & (dist[wirr]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
-        allbox[i]=box/(box+tube+irr)
-        alltube[i]=tube/(box+tube+irr)
-        allirr[i]=1.-(box+tube)/(box+tube+irr)
+            with tables.open_file(dirt+varfile[Ncom],'r') as var:
+                rvir=var.root.Rvir[:]/classify.h*classify.kpc
+                dist=f.root.avgdist[:,whichdist]/rvir
+                C=f.root.classification[:]
+        allbox[i],alltube[i],allirr[i]=calcfrac(C,dist,edges)
 
-    #print allbox
     fig=plt.figure()
-    plt.semilogx(x,np.median(allbox,axis=0),c='k',ls=ls)
-    plt.semilogx(x,np.median(alltube,axis=0),c='b',ls=ls)
-    plt.semilogx(x,np.median(allirr,axis=0),c='r',ls=ls)
+    for w,c in [[allbox,'k'],[alltube,'b'],[allirr,'r']]:
+        plt.semilogx(x,np.median(w,axis=0),c=c,ls=ls)
     plt.legend(['Box','Tube','Irr'],loc=2)
 
     if ploterr==True:
-        plt.fill_between(x,per(allbox,25,axis=0),per(allbox,75,axis=0),color='k',alpha=0.1)
-        plt.fill_between(x,per(alltube,25,axis=0),per(alltube,75,axis=0),color='b',alpha=0.1)
-        plt.fill_between(x,per(allirr,25,axis=0),per(allirr,75,axis=0),color='r',alpha=0.1)
+        for w,c in [[allbox,'k'],[alltube,'b'],[allirr,'r']]:
+            plt.fill_between(x,per(w,25,axis=0),per(w,75,axis=0),color=c,alpha=0.1)
     #plt.axvline(0.5,c='k',ls='--')
     plt.xlabel(r'$r/R_{200}$')
     plt.ylabel('Orbit Fraction')
+    return fig
 
-def pdfmulti(dir,filename='classout3.hdf5'):
+def pdfmulti(dir,components,filename='classout3.hdf5'):
     from matplotlib.backends.backend_pdf import PdfPages
     import matplotlib.pyplot as plt
     import glob
 
     alldir=glob.glob(dir+'/GROUP_*')
 
-    with PdfPages(dir+'multipage_pdf.pdf') as pdf:
+    with PdfPages(dir+'allplots.pdf') as pdf:
+        fig=plotall(dir,components,filename='classout3.hdf5',ploterr=True)
+        pdf.savefig(fig)
+        plt.close()
         for i in alldir:
             print i
-            fig=plotfin(i+'/classout3.hdf5',1)
+            fig=plotfin(i+'/classout3.hdf5',components)
             pdf.savefig(fig)
             plt.close()
+
+def calcfrac(C,dist,edges):
+    wbox=np.where( (C==4) | (C==5))[0]
+    wtube=np.where( (C==1) | (C==2) | (C==3))[0]
+    wirr=np.where(C==0)[0]
+    nbox=np.array([bin.sum() for bin in ((edges[i]<dist[wbox]) & (dist[wbox]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
+    ntube=np.array([bin.sum() for bin in ((edges[i]<dist[wtube]) & (dist[wtube]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
+    nirr=np.array([bin.sum() for bin in ((edges[i]<dist[wirr]) & (dist[wirr]<edges[i+1])  for i in np.arange(len(edges)-1))],dtype='float')
+    fbox=nbox/(nbox+ntube+nirr)
+    ftube=ntube/(nbox+ntube+nirr)
+    firr=1.-(fbox+ftube)
+    return fbox,ftube,firr
 
 if __name__ == "__main__":
     step=''
